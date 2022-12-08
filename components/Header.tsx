39a0d467b3a1a1cea250b434ebbaf5f676a3b22d
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-12-08 13:59:53 +0530

    feat: some updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,17 @@
-import { useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Header() {
-  // Helpful thirdweb hooks to connect and manage the wallet from metamask.
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
-
   return (
     <div className={styles.header}>
       <div className={styles.left}>
         <div>
           <Link href="/">
-            <img
-              src={`/logo.png`}
+            <Image
+              width={135}
+              height={25}
+              src="/logo.png"
               alt="Thirdweb Logo"
               className={styles.headerLogo}
             />
